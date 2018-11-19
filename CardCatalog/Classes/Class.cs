e6eff83a33940bb6e26e3dca7dfd0df1b053e514
Logan Grzesiek <lmgrzesiek@gmail.com>
2018-11-18 22:19:06 -0600

    Finished the application.

---
+++ CardCatalog/Classes/Class.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+ 
+namespace CardCatalog
+{
+    public class CardCatalog
+    {
+        private static string string_filename;
+
+        private static List<Book> books;
+
+        public CardCatalog(string fileName)
+        {
+            //puts a value into string_filename
+            string_filename = fileName + ".csv";
+
+            //if whatever.csv doesn't exist, create a new file
+            if (!File.Exists(string_filename)) 
+            {
+                //ask Joe to elaborate on why we need a using statement to prevent "file already being used" error on save
+                using(var file = File.Create(string_filename))
+                {
+
+                }
+                
+                books = new List<Book>();
+            }
+            else
+            {
+                //using LINQ, we take the long string of info from string_filename and assign it to a variable,v,
+                //and runs it through our fromCSV method...creating a list using the .ToList()
+                books = File.ReadAllLines(string_filename).Select(v => Book.FromCsv(v)).ToList(); 
+            }
+
+            
+        }
+
+        public static void ListBooks()
+        {
+            if (books.Count > 0)
+            {
+                foreach (Book item in books)
+                {
+                    Console.WriteLine("Title: {0}, Author: {1}, Genre: {2}", item.Title, item.Author, item.Genre);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No books have been entered...yet.");
+            }
+        }
+
+        public static void AddBook()
+        {
+            Console.WriteLine("Provide a title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Provide the author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Provide the genre: ");
+            string genre = Console.ReadLine();
+
+            Book book = new Book
+            {
+                Title = title,
+                Author = author,
+                Genre = genre
+            };
+
+            books.Add(book);
+            Console.WriteLine("You have successfully added a book!");
+        }
+
+        public static void Save()
+        {
+            using (StreamWriter file = new StreamWriter(string_filename))
+            {
+                foreach (Book book in books)
+                {
+                    file.WriteLine("{0},{1},{2}", book.Title, book.Author, book.Genre);
+                }
+            }
+
+
+        }
+
+
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+        public static Book FromCsv(string csvLine)
+        {
+            //invoked after csv file is read
+            string[] csvValues = csvLine.Split(","); 
+
+            Book book = new Book
+            {
+                Title = csvValues[0],
+                Author = csvValues[1],
+                Genre = csvValues[2]
+            };
+
+            return book;
+            //populating "books" with book objects from our csv file that already exists
+        }
+    }
+}
--- CardCatalog/Program.cs
+++ CardCatalog/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace CardCatalog
 {
@@ -6,7 +9,51 @@ namespace CardCatalog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool hasSpace = true;
+            while (hasSpace)
+            {
+                Console.WriteLine("What one-word name would you like to call your card catalog?");
+                string fileName = Console.ReadLine();
+                hasSpace = fileName.Contains(" ");
+
+                if (hasSpace)
+                {
+                    Console.WriteLine("I'm sorry, your name fails to meet the one-word requirement, please try again...");
+                }
+                else
+                {
+                    Console.WriteLine("Thank you! Please choose an option from the menu below:");
+                    new CardCatalog(fileName);
+                }
+            }
+            string userChoice = MainMenu();
+
+            while (userChoice != "3")
+            {
+                if (userChoice == "1")
+                {
+                    CardCatalog.ListBooks();
+                    userChoice = MainMenu();
+                }
+                else if (userChoice == "2")
+                {
+                    CardCatalog.AddBook();
+                    userChoice = MainMenu();
+                }
+            }
+            CardCatalog.Save();
+
+        }
+        
+
+        private static string MainMenu()
+        {
+           //error when 1, 2 or 3 is not given 
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) List All Books");
+            Console.WriteLine("2) Add A Book");
+            Console.WriteLine("3) Save and Exit");
+            return Console.ReadLine();
         }
     }
 }
